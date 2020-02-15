b4a6ef5f1afa78892f809cfb43b92a17f933d498
Tanvir Ahamed <49130408+tanvirkse101@users.noreply.github.com>
2020-02-15 17:47:46 +0600

    update

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-        /* Set the width of the sidebar to 150px and the left margin of the page content to 150px */
-        function openNav() {
-            document.getElementById("mySidebar").style.width = "200px";
-        }
-
-        /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-        function closeNav() {
-            document.getElementById("mySidebar").style.width = "0";
-        }
+ /* Set the width of the sidebar to 150px and the left margin of the page content to 150px */
+ function openNav() {
+     document.getElementById("mySidebar").style.width = "200px";
+ }
+ /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+ function closeNav() {
+     document.getElementById("mySidebar").style.width = "0";
+ }
