36af05a13ece9bdbe30df2afaf65a4a1cc696c5e
Tanvir Ahamed <49130408+tanvirkse101@users.noreply.github.com>
2020-02-29 16:10:10 +0600

    update

---
+++ php/db_connect.php
@@ -0,0 +1,12 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "university_review";
+$conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection failed: " . mysqli_connect_error());
+if ($conn) {
+    echo "Connected successfully";
+}
+
+?>
