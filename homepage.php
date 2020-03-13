a25745fb55f152d0ab748b9ae180573178079f52
Tanvir Ahamed <49130408+tanvirkse101@users.noreply.github.com>
2020-03-13 09:46:18 +0600

    fixes&search

--- homepage.php
+++ homepage.php
@@ -14,6 +14,9 @@
     <script src="js/script.js"></script>
     <!-- Load an icon library -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+    <script type="text/javascript">
+    </script>
 </head>
 
 <body>
@@ -41,10 +44,7 @@
         <a class="navbar-brand" href="#">Uni-Review</a>
 
         <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-            <form class="example" action="#">
-                <input type="text" placeholder="Search.." name="search">
-                <button type="submit"><i class="fa fa-search"></i></button>
-            </form>
+
         </nav>
 
         <!-- Links -->
@@ -71,11 +71,23 @@
                     data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-fw fa-cogs"></i>
                     Settings</a>
                 <div class="dropdown-menu" aria-labelledby="dropdown01">
-                    <a class="dropdown-item" href="userprofile.php">Edit Profile</a>
-                    <a class="dropdown-item" href="php/logout.php">logout</a>
-                    <a class="dropdown-item" href="aboutpage.php">About</a>
-                    <a class="dropdown-item" href="adminDashboard.php">Admin Dashboard</a>
-                    <a class="dropdown-item" href=""> Contact Us</a>
+                    <?php
+                    if(isset($_SESSION["name"])) 
+                    {
+                    echo '<a class="dropdown-item" href="userprofile.php">Edit Profile</a>
+                         <a class="dropdown-item" href="php/logout.php">logout</a>
+                         <a class="dropdown-item" href="aboutpage.php">About</a>
+                         <a class="dropdown-item" href="adminDashboard.php">Admin Dashboard</a>
+                         <a class="dropdown-item" href=""> Contact Us</a>';
+                    }
+                    else
+                    {
+                        echo '<a class="dropdown-item" href="signup.php">Register</a>
+                              <a class="dropdown-item" href="aboutpage.php">About</a>
+                              <a class="dropdown-item" href="#">Contact Us</a>';
+                    }
+                    ?>
+
                 </div>
             </li>
 
@@ -88,6 +100,36 @@
 
         <div class="jumbotron bg-primary shadow-lg">
             <h1 class="display-4" style="color:white;text-align: center;">University List</h1>
+            <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+            <script type="text/javascript">
+                $(document).ready(function () {
+                    $('.search-box input[type="text"]').on("keyup input", function () {
+                        /* Get input value on change */
+                        var inputVal = $(this).val();
+                        var resultDropdown = $(this).siblings(".result");
+                        if (inputVal.length) {
+                            $.get("backend-search.php", {
+                                term: inputVal
+                            }).done(function (data) {
+                                // Display the returned data in browser
+                                resultDropdown.html(data);
+                            });
+                        } else {
+                            resultDropdown.empty();
+                        }
+                    });
+
+                    // Set search input value on click of result item
+                    $(document).on("click", ".result p", function () {
+                        $(this).parents(".search-box").find('input[type="text"]').val($(this).text());
+                        $(this).parent(".result").empty();
+                    });
+                });
+            </script>
+            <div class="search-box text-center">
+                <input type="text" autocomplete="off" placeholder="Search country..." />
+                <div class="result"></div>
+            </div>
         </div>
         <?php 
                 $sql = 'SELECT id, name, location, rating, img_src FROM university';
@@ -99,14 +141,14 @@
         <div class="card-columns">
 
             <?php foreach($info as $info) { ?>
-                <!-- <?php echo $info['img_src'];?>"> -->
 
             <div class="card shadow border rounded" style="width:356px;height:402px">
-                <img class="card-img-top" style="padding: 8px;" src="upload/university/<?php echo $info['img_src'];?>" alt="">
+                <img class="card-img-top mx-auto d-block" style="padding:3px; height: 200px;width: 353px;"
+                    src="upload/university/<?php echo $info['img_src'];?>" alt="<?php echo $info['name'];?>">
                 <div class="card-body">
                     <ul class="list-group list-group-flush">
-                        <li class="list-group-item"><b><a style="color: black;"
-                                    href="university.php?<?php echo "uni_id="; ?><?php echo $info['id'];?>"><?php echo htmlspecialchars($info['name']); ?>
+                        <li class="list-group-item"><b><a
+                                    style="color: black;"><?php echo htmlspecialchars($info['name']); ?>
                                     <??>
 
                                 </a></b>
@@ -117,10 +159,11 @@
                             <span class="fa fa-star checked"></span>
                             <span class="fa fa-star checked"></span>
                             <span class="fa fa-star-half checked"></span> <a style="margin-left: 10px;"
-                                href="#"><?php echo htmlspecialchars($info['rating']); ?>
-                            </a>
+                                href="#"><?php echo htmlspecialchars($info['rating']); ?></a>
                         </li>
-                        <li class="list-group-item"><a href="#"> Click for more info -> </a></li>
+                        <li class="list-group-item"><a
+                                href="university.php?<?php echo "uni_id="; ?><?php echo $info['id'];?>"> Click for more
+                                info -> </a></li>
                     </ul>
                 </div>
             </div>
@@ -133,10 +176,8 @@
 
     <!--Main Body End-->
 
-    <!--Bootstrap JS,JSQ-->
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
-        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
-    </script>
+    <!--Bootstrap JS,JQ-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
         integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
     </script>
